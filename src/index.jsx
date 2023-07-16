c30ef059b5a993a04f35b84b9664e9327b0b34bf
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-07-16 12:23:55 +0000

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <div>
+      ⚛️
+      <h1>
+        Fun Facts About React
+      </h1>
+      <ul>
+        <li><b>React is not a framework.</b></li>
+        <li><b>React has a strong community.</b></li>
+        <li><b>Virtual DOM is faster than real DOM.</b></li>
+        <li><b>React and SEO Go Together.</b></li>
+        <li><b>You Can Create a Fully-Fledged Project With a Single Command.</b></li>
+        <li><b>React Projects Can be Started With a Specific Template.</b></li>
+        
+      </ul>
+    </div>
+  </React.StrictMode>
+)
